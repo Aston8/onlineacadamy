b8a9fd8fdc077cdba2ea9d96e33cf17d22304594
Aston Monteiro <astonmonteiro08@gmail.com>
2024-08-07 23:40:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -11,6 +14,9 @@ export default function Home() {
         <FeaturedCourses />
         <WhyChooseUs />
         <MusicSchoolTestimonials />
+        <UpcomingWebinars />
+        <Instructors />
+        <Footer />
       </main>
     );
   }
