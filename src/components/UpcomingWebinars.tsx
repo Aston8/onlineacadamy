0627d39d2c6d548732fa1b95f4f2e103e79242d3
Aston Monteiro <astonmonteiro08@gmail.com>
2024-09-06 10:58:43 +0530

    Update UpcomingWebinars.tsx

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -71,7 +71,7 @@ import { HoverEffect } from "./ui/card-hover-effect";
         </div>
 
         <div className="mt-10 text-center">
-         <Link href={"/"}
+         <Link href={"/webinars"}
          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
          >
             View all webinars
